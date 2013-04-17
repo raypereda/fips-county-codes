83cbf95b0a6fe20a864071462dde3ffb8d50a683
Ray Pereda <ray@nationbuilder.com>
2013-04-17 11:53:20 -0700

    Initial commit

---
+++ lib/nist_codes.rb
@@ -0,0 +1,27 @@
+module NistCodes
+  fips = {}
+  state_county = {}
+
+  File.open('lib/national.txt') do |f|
+    f.readline # skip the first line with headers
+
+    until f.eof
+      row = f.readline.chomp.split(',')
+      state, state_code, county_code, county, class_code_ignored = row
+
+      fips[state] = {} if not fips.member?(state)
+
+      fips_code = (state_code + county_code).to_i
+      fips[state][county] = fips_code
+
+      county2 = county.gsub(' County', '')
+      fips[state][county2] = fips_code if county != county2
+
+      state_county[fips_code] = [state, county]
+    end
+  end
+
+  FIPS = fips
+  STATE_COUNTY = state_county
+end
+
